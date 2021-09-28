98f6520be9266faf32dbeb51e9c03c4927f7a3c3
BrunoC300 <brunocunha57@gmail.com>
2021-09-28 16:58:08 +0100

    display tickets pendentes

--- resources/views/site/index.blade.php
+++ resources/views/site/index.blade.php
@@ -1,22 +1,68 @@
-<h1>Tickets</h1>
-<hr>
-<table>
-    <tr>
-        <th>Descricao</th>
-        <th>ValorPrevisto</th>
-        <th>EmailResponsavel</th>
-    </tr>
-    @foreach ($pendentes as $pendente)
-        <tr>
-            <td>{{ $pendente->descricao }}</td>
-            <td>{{ $pendente->valorPrevisto }}€</td>
-            <td>{{ $pendente->emailResponsavel }}</td>
-        </tr>
-    @endforeach
-</table>
-<h3><a> Adicionar outro ticket</a></h3>
-{{-- {{ $tickets->links() }} --}}
+<!DOCTYPE html>
+<html lang="en">
 
-<hr>
+<head>
+    <title>Tickets</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="/vendor/perfect-scrollbar/perfect-scrollbar.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" href="{{ asset('css/ticket.css') }}">
+    <!--===============================================================================================-->
+</head>
 
-{{-- <p><a href="{{ route('app.sair') }}">LOGOUT</a></p> --}}
+<body>
+    <div class="limiter">
+        <div class="container-table100">
+            <div class="wrap-table100">
+                <h1 class="title__header">Tickets Pendentes</h1>
+                <div class="table100">
+                    <table>
+                        <thead>
+                            <tr class="table100-head">
+                                <th class="column1">Descrição</th>
+                                <th class="column2">Valor Previsto</th>
+                                <th class="column3">Responsável</th>
+
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($pendentes as $pendente)
+                                <tr>
+                                    <td class="column1">{{ $pendente->descricao }}</td>
+                                    <td class="column2">{{ $pendente->valorPrevisto }}€</td>
+                                    <td class="column3">{{ $pendente->emailResponsavel }}</td>
+                                </tr>
+                            @endforeach
+                            </tr>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+    {{-- <p><a href="{{ route('app.sair') }}">LOGOUT</a></p> --}}
+
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+
+</body>
+
+</html>
